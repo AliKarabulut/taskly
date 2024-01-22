d426e769f292decb74e163a06c5f896478c8a714
alikarabulut <a.kbt6666@gmail.com>
2024-01-22 03:39:45 +0300

    refactor: form error and succes style

--- src/components/form-error/index.tsx
+++ src/components/form-error/index.tsx
@@ -1,9 +1,15 @@
+import Exclamation from '@/components/icons/exclamation'
 type FormErrorProps = {
   message: string
 }
 
 const FormError = ({ message }: FormErrorProps) => {
-  return <div>{message}</div>
+  return (
+    <div className="mb-1.5 inline-flex w-full items-center justify-center gap-x-1.5 rounded-md bg-red-100 px-1.5 py-1 text-sm font-medium text-red-700">
+      <Exclamation />
+      {message}
+    </div>
+  )
 }
 
 export default FormError
--- src/components/form-success/index.tsx
+++ src/components/form-success/index.tsx
@@ -1,9 +1,16 @@
+import Check from '@/components/icons/check'
+
 type FormSuccessProps = {
   message: string
 }
 
 const FormSuccess = ({ message }: FormSuccessProps) => {
-  return <div>{message}</div>
+  return (
+    <div className="mb-1.5 inline-flex w-full items-center justify-center gap-x-1.5 rounded-md bg-green-100 px-1.5 py-1 text-sm font-medium text-green-700">
+      <Check />
+      {message}
+    </div>
+  )
 }
 
 export default FormSuccess
