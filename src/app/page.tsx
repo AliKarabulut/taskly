7506c254818eeb42cfb3b7bba84f4e83404443d7
alikarabulut <a.kbt6666@gmail.com>
2024-02-05 23:49:56 +0300

    Add content to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
 const Home = () => {
-  return <main>Home</main>
+  return (
+    <main className="my-auto">
+      <div className="m-auto max-w-2xl rounded-md px-8 py-10 shadow">
+        This project is created for the purpose of learning Next Auth. The addition of a todo feature is akin to adding sugar to tea – a
+        simple and elegant enhancement to the existing functionality.
+      </div>
+    </main>
+  )
 }
 export default Home
