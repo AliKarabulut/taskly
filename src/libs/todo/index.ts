c64f8ac3ffa6c7f7f122ee8662c3b38e32f30398
alikarabulut <a.kbt6666@gmail.com>
2024-02-07 02:11:21 +0300

    todo pagination

--- src/libs/todo/index.ts
+++ src/libs/todo/index.ts
@@ -14,12 +14,14 @@ export const getTodoById = async (id: string) => {
   }
 }
 
-export const getTodoByUserId = async (userId: string) => {
+export const getTodoByUserId = async (userId: string, page: number) => {
   try {
     const todo = await client.todo.findMany({
       where: {
         userId,
       },
+      skip: page * 10,
+      take: 10,
     })
 
     return todo
