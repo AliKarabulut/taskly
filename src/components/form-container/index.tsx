9fc4adc89d5b7602fd472b1be78dc711e0a0e94f
alikarabulut <a.kbt6666@gmail.com>
2024-02-14 02:34:52 +0300

    Refactor form container layout

--- src/components/form-container/index.tsx
+++ src/components/form-container/index.tsx
@@ -7,13 +7,8 @@ const FormContainer = ({ title, children }: FormContainerProps) => {
   return (
     <section>
       <div className="flex min-h-full flex-1 flex-col items-center justify-center sm:px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">{title}</h2>
-        </div>
-
-        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-[480px]">
-          <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">{children}</div>
-        </div>
+        <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">{title}</h2>
+        <div className="mt-8 bg-white px-6 py-12 shadow sm:mx-auto sm:w-full sm:max-w-[480px] sm:rounded-lg sm:px-12">{children}</div>
       </div>
     </section>
   )
