065ed047e1f4dc8654ef90e673088b938092c1d0
Ali Karabulut <56300204+AliKarabulut@users.noreply.github.com>
2024-06-26 23:55:26 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ const Home = () => {
   return (
     <main className="my-auto">
       <div className="m-auto max-w-2xl rounded-md px-8 py-10 shadow dark:text-darkModeNeutral-100">
-        This project is created for the purpose of learning Next Auth. The addition of a todo feature is akin to adding sugar to tea – a
-        simple and elegant enhancement to the existing functionality.
+        This project was created to learn Next Auth. To make the project more than just about authentication, I added a todo feature to 
+        enhance its functionality. This addition made the project more useful and contributed to the learning process.
       </div>
     </main>
   )
